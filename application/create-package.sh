26a210e1da6daab4b0bfba268a5d7ec610bae9d8
LoganBrownGU <2641407b@student.gla.ac.uk>
2026-04-10 15:21:27 +0100

    Added version to package
    Fixed spelling error

--- application/create-package.sh
+++ application/create-package.sh
@@ -5,7 +5,11 @@ PACKAGENAME=digitsynth-aarch64
 cd build/
 mkdir -p $PACKAGENAME/DEBIAN/
 mkdir -p $PACKAGENAME/usr/bin/ 
+
 cp control.in $PACKAGENAME/DEBIAN/control
+version="$(cat ../VERSION)-$(git rev-parse HEAD)"
+sed -i "s/VERSION/$version/g" $PACKAGENAME/DEBIAN/control 
+
 cp digitsynth $PACKAGENAME/usr/bin/
 dpkg-deb --root-owner-group --build $PACKAGENAME
 
